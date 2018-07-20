a489b40d1894745469d4cb7479feae7751edaa9e
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-07-20 11:14:16 +1000

    Format DataDog event message.

--- send-datadog-event.sh
+++ send-datadog-event.sh
@@ -23,5 +23,6 @@ END
 
 
 echo "Generating DataDog event"
-dog event post --no_host --tags $TAGS --type travis "New $APP event created" "$TRAVIS_REPO_SLUG: new build (no $TRAVIS_BUILD_NUMBER) on branch $TRAVIS_BRANCH succeeded for commit $TRAVIS_COMMIT"
+event_txt="$TRAVIS_REPO_SLUG: new build (no [$TRAVIS_BUILD_NUMBER](https://travis-ci.org/umccr/test-vault-secrets-injection/builds/$TRAVIS_BUILD_ID)) on branch $TRAVIS_BRANCH succeeded for commit [${TRAVIS_COMMIT:0:12}](https://github.com/umccr/test-vault-secrets-injection/commit/${TRAVIS_COMMIT:0:12})"
+dog event post --no_host --tags $TAGS --type travis "New $APP event created" "$event_text"
 echo "Event successfully sent."
