6fb972f88758c68a753640515dfc91d8c194ae5a
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-07-20 11:51:51 +1000

    Switch from dogshell to curl

--- send-datadog-event.sh
+++ send-datadog-event.sh
@@ -5,24 +5,22 @@ set -o pipefail
 APP=$1
 TAGS=$2
 
-# install DataDog Python library
-pip install datadog
-
 export VAULT_ADDR=$VAULT_ADDR_DEV
 export VAULT_TOKEN=$VAULT_TOKEN_DEV
 
 DATADOG_API_KEY=$(vault kv get -format=json kv/datadog | jq -r '.data.api_key')
-DATADOG_APP_KEY=$(vault kv get -format=json kv/datadog | jq -r '.data.app_key')
-
-# Set up
-cat > ~/.dogrc << END
-[Connection]
-apikey = ${DATADOG_API_KEY}
-appkey = ${DATADOG_APP_KEY}
-END
-
 
 echo "Generating DataDog event"
-event_txt="%%% \n $TRAVIS_REPO_SLUG: new build (no [$TRAVIS_BUILD_NUMBER](https://travis-ci.org/umccr/test-vault-secrets-injection/builds/$TRAVIS_BUILD_ID)) on branch $TRAVIS_BRANCH succeeded for commit [${TRAVIS_COMMIT:0:12}](https://github.com/umccr/test-vault-secrets-injection/commit/${TRAVIS_COMMIT}) \n %%%"
-dog event post --no_host --tags $TAGS --type travis "New $APP event created" "$event_txt"
+event_text="%%% \n $TRAVIS_REPO_SLUG: new build (no [$TRAVIS_BUILD_NUMBER](https://travis-ci.org/umccr/test-vault-secrets-injection/builds/$TRAVIS_BUILD_ID)) on branch $TRAVIS_BRANCH succeeded for commit [${TRAVIS_COMMIT:0:12}](https://github.com/umccr/test-vault-secrets-injection/commit/${TRAVIS_COMMIT}) \n %%%"
+
+curl -X POST -H "Content-type: application/json" \
+-d "{
+      \"title\": \"New $APP event created\",
+      \"text\": \"$event_text\",
+      \"priority\": \"normal\",
+      \"tags\": [\"$TAGS\"],
+      \"alert_type\": \"info\"
+}" \
+"https://api.datadoghq.com/api/v1/events?api_key=$DATADOG_API_KEY"
+
 echo "Event successfully sent."
