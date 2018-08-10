f89a5c4e1a28dfd134576694da07f10a5abba49e
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-08-10 11:44:47 +1000

    Merge master

--- push-to-docker-hub.sh
+++ push-to-docker-hub.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 set -eo pipefail
 
+# expect to get the docker image name provided
+DOCKER_IMAGE=$1
+
 DOCKER_USER=$(vault kv get -format=json kv/docker | jq -r '.data.docker_user')
 DOCKER_PASSWORD=$(vault kv get -format=json kv/docker | jq -r '.data.docker_password')
 
+if [[ "$TRAVIS_BRANCH" =~ [0-9]\.[0-9]+(\.[0-9]+)?(-[A-Za-z0-9_])? ]]; then
+  DOCKER_TAG="$DOCKER_IMAGE:$TRAVIS_BRANCH"
+else
+  DOCKER_TAG="$DOCKER_IMAGE:latest"
+fi
+echo "Building $DOCKER_TAG"
+
 docker version
-docker build --no-cache -t umccr/test-vault-secrets-injection .
+docker build --no-cache -t "$DOCKER_TAG" .
+
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin
-docker push umccr/test-vault-secrets-injection
+docker push "$DOCKER_TAG"
